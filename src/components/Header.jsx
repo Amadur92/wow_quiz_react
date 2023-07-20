aac8d0d3a02de10d2da564be35d0efb19f89c9b3
Mike Sokolenko <69795882+Amadur92@users.noreply.github.com>
2023-07-20 12:36:39 +0400

    dialogs add

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import react from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'> 
+    <img className='logo' src='https://kirov.wowquiz.ru/images/logo-white.png' alt='logo'></img>
+    hello asshole
+    
+    </header>
+    )
+}
+
+export default Header
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Navbar = () => {
+    return (<nav className='sidebar'>
+    <div>
+    <ul>one</ul>
+    </div>
+    <div>
+    <ul>two</ul>
+    </div>
+    <div>
+    <ul>three</ul>
+    </div>
+    </nav>)
+
+}
+
+export default Navbar
